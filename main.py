6b28181416cc50cc47b63a1fa54c9cf855f35ec0
Nick Alico <nro337@gmail.com>
2026-02-11 01:06:30 -0500

    Update prefs

--- main.py
+++ main.py
@@ -6,40 +6,41 @@ import feedparser
 import markdownify
 
 # Define your Zulip credentials
-ZULIP_EMAIL = os.environ.get('ZULIP_EMAIL')
-ZULIP_STREAM_NAME = 'articles'
+ZULIP_EMAIL = os.environ.get("ZULIP_EMAIL")
+ZULIP_STREAM_NAME = "rss"
 
 # Define the dictionary of feed names and their RSS links
 RSS_FEEDS = {
+    "HN": "https://news.ycombinator.com/rss",
     # "arXiv OA and FA": "https://rss.arxiv.org/atom/math.OA+math.FA", doesn't have published_parsed data
-    "Dan Ma's Topology Blog": "https://dantopology.wordpress.com/feed/",
-    "Orr Shalit": "https://noncommutativeanalysis.wordpress.com/feed/",
-    "Free Probability Theory": "https://rolandspeicher.com/feed/",
-    "The Higher Geometer": "https://thehighergeometer.wordpress.com/feed/",
-    "Terence Tao": "https://terrytao.wordpress.com/feed/",
-    "Peter Woit": "https://www.math.columbia.edu/~woit/wordpress/atom.xml",
-    "Math3ma": "http://www.math3ma.com/blog/rss.xml",
-    "Math ∩ Programming": "https://www.jeremykun.com/index.xml",
-    "Ramis Movassagh": "https://ramismovassagh.wordpress.com/blog/feed/",
-    "DESVL": "https://desvl.xyz/atom.xml",
-    "George Lowther": "https://almostsuremath.com/feed",
-    "n-Category Cafe": "https://golem.ph.utexas.edu/category/atom10.xml",
-    "Mathematics, Quanta Magazine": "https://api.quantamagazine.org/mathematics/feed",
-    "Physics, Quanta Magazine": "https://api.quantamagazine.org/physics/feed",
-    "Biology, Quanta Magazine": "https://api.quantamagazine.org/biology/feed",
-    "Computer Science, Quanta Magazine": "https://api.quantamagazine.org/computer-science/feed",
-    "Steven Wolfram, Writings": "https://writings.stephenwolfram.com/feed",
-    "Bit Player": "http://bit-player.org/feed",
-    "Geoff Boeing": "https://geoffboeing.com/feed/",
-    "Azimuth": "https://johncarlosbaez.wordpress.com/feed",
-    "Arch Linux": "https://archlinux.org/feeds/news/",
+    # "Dan Ma's Topology Blog": "https://dantopology.wordpress.com/feed/",
+    # "Orr Shalit": "https://noncommutativeanalysis.wordpress.com/feed/",
+    # "Free Probability Theory": "https://rolandspeicher.com/feed/",
+    # "The Higher Geometer": "https://thehighergeometer.wordpress.com/feed/",
+    # "Terence Tao": "https://terrytao.wordpress.com/feed/",
+    # "Peter Woit": "https://www.math.columbia.edu/~woit/wordpress/atom.xml",
+    # "Math3ma": "http://www.math3ma.com/blog/rss.xml",
+    # "Math ∩ Programming": "https://www.jeremykun.com/index.xml",
+    # "Ramis Movassagh": "https://ramismovassagh.wordpress.com/blog/feed/",
+    # "DESVL": "https://desvl.xyz/atom.xml",
+    # "George Lowther": "https://almostsuremath.com/feed",
+    # "n-Category Cafe": "https://golem.ph.utexas.edu/category/atom10.xml",
+    # "Mathematics, Quanta Magazine": "https://api.quantamagazine.org/mathematics/feed",
+    # "Physics, Quanta Magazine": "https://api.quantamagazine.org/physics/feed",
+    # "Biology, Quanta Magazine": "https://api.quantamagazine.org/biology/feed",
+    # "Computer Science, Quanta Magazine": "https://api.quantamagazine.org/computer-science/feed",
+    # "Steven Wolfram, Writings": "https://writings.stephenwolfram.com/feed",
+    # "Bit Player": "http://bit-player.org/feed",
+    # "Geoff Boeing": "https://geoffboeing.com/feed/",
+    # "Azimuth": "https://johncarlosbaez.wordpress.com/feed",
+    # "Arch Linux": "https://archlinux.org/feeds/news/",
 }
 
 # Function to send a message to Zulip
 
 
 def send_zulip_message(content, topic):
-    client = zulip.Client(email=ZULIP_EMAIL, client='rss-feed-bot/0.1')
+    client = zulip.Client(email=ZULIP_EMAIL, client="rss-feed-bot/0.1")
     data = {
         "type": "stream",
         "to": ZULIP_STREAM_NAME,
@@ -48,11 +49,12 @@ def send_zulip_message(content, topic):
     }
     client.send_message(data)
 
+
 # Get the url of the last article update sent to the stream
 
 
 def last_article_update_link(topic):
-    client = zulip.Client(email=ZULIP_EMAIL, client='test-github-client/0.1')
+    client = zulip.Client(email=ZULIP_EMAIL, client="test-github-client/0.1")
     request: Dict[str, Any] = {
         "anchor": "newest",
         "num_before": 1,
@@ -62,23 +64,23 @@ def last_article_update_link(topic):
             {"operator": "stream", "operand": ZULIP_STREAM_NAME},
             {"operator": "topic", "operand": topic},
         ],
-        "apply_markdown": False
+        "apply_markdown": False,
     }
     response = client.get_messages(request)
-    if response['result'] == "success":
+    if response["result"] == "success":
         messages = response["messages"]
         if messages:
             latest_message = messages[0]
             latest_message_content = latest_message["content"]
-            url_pattern = r'\[.*?\]\((.*?)\)'
-            latest_arxiv_link = re.findall(
-                url_pattern, latest_message_content)[0]
+            url_pattern = r"\[.*?\]\((.*?)\)"
+            latest_arxiv_link = re.findall(url_pattern, latest_message_content)[0]
             return latest_arxiv_link
         else:
             return None
     else:
         print(f"Failed to retrieve message or No previous messages")
         return None
+
 
 # Function to fetch latest articles from arXiv
 
@@ -98,14 +100,22 @@ def update_zulip_stream():
                 if link == last_updated_article_link:
                     flag = False
                 continue
-            title = markdownify.markdownify(article.title.replace('$^{\\ast}$', '* ').replace(
-                '$^*$', '* ').replace('$^*$', '* ').replace("$", "$$").replace("\n", " "))
+            title = markdownify.markdownify(
+                article.title.replace("$^{\\ast}$", "* ")
+                .replace("$^*$", "* ")
+                .replace("$^*$", "* ")
+                .replace("$", "$$")
+                .replace("\n", " ")
+            )
             published = time.strftime("%d %B %Y", article.published_parsed)
             # Some rss feeds lack these fields
-            author = article.author+", " if 'author' in article.keys() else ""
+            author = article.author + ", " if "author" in article.keys() else ""
             summary = markdownify.markdownify(article.description)
-            tags = ", ".join([entry['term'] for entry in article.tags]
-                             ) if 'tags' in article else ""  # Some feeds lack tags
+            tags = (
+                ", ".join([entry["term"] for entry in article.tags])
+                if "tags" in article
+                else ""
+            )  # Some feeds lack tags
             message = f"\n**[{title}]({link})**\n*{author}{published}*\n\n{summary}\n\n*{tags}*"
             print(message)
             send_zulip_message(message, topic)
